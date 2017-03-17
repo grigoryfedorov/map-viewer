ca6ffde7fffe3404285e286aa08a7c3df06ca903
Grigory Fedorov <gregory.230101@gmail.com>
2017-03-18 01:00:42 +0500

    save current coordinates between view recreations.

--- app/src/main/java/ru/grigoryfedorov/mapview/MapView.java
+++ app/src/main/java/ru/grigoryfedorov/mapview/MapView.java
@@ -2,7 +2,10 @@ package ru.grigoryfedorov.mapview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.graphics.Rect;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -97,6 +100,66 @@ public class MapView extends View {
         scrollController.onDraw();
 
         tileDrawer.drawTiles(canvas, scrollController.getCurrentCoordinates());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        //begin boilerplate code so parent classes can restore state
+        if(!(state instanceof SavedState)) {
+            super.onRestoreInstanceState(state);
+            return;
+        }
+
+        SavedState savedState = (SavedState)state;
+        super.onRestoreInstanceState(savedState.getSuperState());
+        //end
+
+        scrollController.setCurrentCoordinates(
+                savedState.savedCoordinates.x,
+                savedState.savedCoordinates.y);
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        //begin boilerplate code that allows parent classes to save state
+        Parcelable superState = super.onSaveInstanceState();
+
+        SavedState savedState = new SavedState(superState);
+        //end
+
+        savedState.savedCoordinates = scrollController.getCurrentCoordinates();
+
+        return savedState;
+    }
+
+    private static class SavedState extends BaseSavedState {
+        Point savedCoordinates;
+
+        SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        private SavedState(Parcel in) {
+            super(in);
+            this.savedCoordinates = in.readParcelable(getClass().getClassLoader());
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeParcelable(savedCoordinates, 0);
+        }
+
+        //required field that makes Parcelables from a Parcel
+        public static final Parcelable.Creator<SavedState> CREATOR =
+                new Parcelable.Creator<SavedState>() {
+                    public SavedState createFromParcel(Parcel in) {
+                        return new SavedState(in);
+                    }
+                    public SavedState[] newArray(int size) {
+                        return new SavedState[size];
+                    }
+                };
     }
 
 }
